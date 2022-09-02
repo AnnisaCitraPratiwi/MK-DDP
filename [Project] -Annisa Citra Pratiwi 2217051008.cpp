2d8ad935fff1336e8109caf644232f27ce0cf73a
AnnisaCitraPratiwi <112589538+AnnisaCitraPratiwi@users.noreply.github.com>
2022-09-02 09:17:31 +0700

    Add files via upload

---
+++ [Project] -Annisa Citra Pratiwi 2217051008.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main (){
+	string a;
+	cout<<"Nama :";
+	getline(cin,a);
+ 
+	string b;
+	cout<<"NPM :";
+	getline(cin,b);
+ 
+	string c;
+	cout<<"Alamat :";
+	getline(cin,c);
+ 
+	string d;
+	cout<<"Hobi :";
+	getline(cin,d);
+
+	
+		cout<<"Masukan Nama :" <<a<<endl;
+		cout<<"Masukan NPM :" <<b<<endl;
+		cout<<"Masukan Alamat :" <<c<<endl;
+		cout<<"Masukan Hobi :" <<d<<endl;	
+}
